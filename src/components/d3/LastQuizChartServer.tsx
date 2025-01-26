a7f488e862a5e4754b56b8ded9e4afcc80950b9d
sam-davi <164131866+sam-davi@users.noreply.github.com>
2025-01-26 13:40:13 +1300

    added double stars to bar chart
    stars now appear on the top of the bar for double rounds
    if the round has the-list or ponderous rounds these will show up as "L" or "P" instead of the round number

--- src/components/d3/LastQuizChartServer.tsx
+++ src/components/d3/LastQuizChartServer.tsx
@@ -26,6 +26,9 @@ export default async function LastQuizChart({ team }: { team: string }) {
   );
 }
 
+const star =
+  "M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z";
+
 async function LastQuizChartInner({ rounds }: { rounds: DataPoint[] }) {
   if (rounds.length === 0) return null;
 
@@ -46,21 +49,28 @@ async function LastQuizChartInner({ rounds }: { rounds: DataPoint[] }) {
     .range([height - margin.bottom - margin.top, 0]);
 
   const colorScale = d3.scaleSequential(d3.interpolateCool).domain([0, 100]);
+  let roundNumber = 0;
+  const roundKeys = rounds.map((round) => {
+    if (round.category.slug === "the-list") return "L";
+    if (round.category.slug === "ponderous") return "P";
+    roundNumber += 1;
+    return roundNumber.toString();
+  });
 
   return (
     <div className="aspect-2/1 w-full">
       <svg className="h-full w-full overflow-visible">
         {/* X axis */}
-        {rounds.map((round) => (
+        {rounds.map((round, i) => (
           <g key={round.roundNumber} className="text-xs">
             <text
               x={`${(((xScale(round.roundNumber.toString()) ?? 0) + margin.left + xScale.bandwidth() / 2) / width) * 100}%`}
-              y={"100%"}
+              y={`${((height - margin.bottom / 4) / height) * 100}%`}
               textAnchor="middle"
               alignmentBaseline="middle"
               fill="currentColor"
             >
-              {round.roundNumber}
+              {roundKeys[i]}
             </text>
           </g>
         ))}
@@ -72,19 +82,19 @@ async function LastQuizChartInner({ rounds }: { rounds: DataPoint[] }) {
             .map((tick, i) => (
               <text
                 key={i}
-                x={0}
+                x={`${(margin.left / width / 4) * 100}%`}
                 y={`${((yScale(+tick) + margin.top) / height) * 100}%`}
                 alignmentBaseline="middle"
                 textAnchor="end"
                 className="text-xs tabular-nums"
                 fill="currentColor"
               >
-                {tick}
+                {tick}%
               </text>
             ))}
         </g>
         {/* Chart area */}
-        <svg viewBox={`0 0 ${width} ${height}`}>
+        <svg viewBox={`0 0 ${width} ${height}`} overflow="visible">
           <g transform={`translate(${margin.left}, ${margin.top})`}>
             {/* Grid lines */}
             {yScale
@@ -122,6 +132,21 @@ async function LastQuizChartInner({ rounds }: { rounds: DataPoint[] }) {
                 fill={colorScale(round.percentScore ?? 0)}
               />
             ))}
+
+            {/* Stars */}
+            {rounds
+              .filter((round) => round.double)
+              .map((round) => (
+                <path
+                  key={round.roundNumber}
+                  d={star}
+                  fill={"gold"}
+                  transform={`translate(${
+                    (xScale(round.roundNumber.toString()) ?? 0) +
+                    xScale.bandwidth() / 4.5
+                  }, ${yScale(round.percentScore ?? 0) - 21})`}
+                />
+              ))}
           </g>
         </svg>
       </svg>
