be51c21cc09c0f9a5a553d00426ef70220238845
sam-davi <164131866+sam-davi@users.noreply.github.com>
2025-03-05 15:46:47 +1300

    added dark/light theme switcher

--- src/app/(dashboard)/quiz/[team]/layout.tsx
+++ src/app/(dashboard)/quiz/[team]/layout.tsx
@@ -1,5 +1,6 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import NavBreadcrumbs from "@/components/nav-breadcrumbs";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 import {
   SidebarInset,
   SidebarProvider,
@@ -15,11 +16,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
       <AppSidebar teams={getTeamsAction()} user={getUserProfileAction()} />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
             <NavBreadcrumbs />
+          </div>
+          <div className="flex items-center gap-2 px-4">
+            <ThemeSwitcher />
           </div>
         </header>
         {children}
